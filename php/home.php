49223d7901b99cd77779b94e7eab50d481bddb55
Fabio Colella <fcole90@gmail.com>
2015-04-15 21:27:36 +0200

    Added pictures of packages and other resources

---
+++ php/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Fisherman Locator</title>
+	</head>
+
+	<body>
+		<h1>Fisherman Locator</h1>
+		<p> <?php echo time(); ?> </p>
+		
+		<p>
+			Fisherman Locator allows you to find the Fisherman's 
+			Friend logenzes in the shops in the nearby.
+		</p>
+		
+	</body>
+
+</html>
