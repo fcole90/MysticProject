9b5df89c28cd8fc98910473cd39502a26deb611a
Fabio Colella <fcole90@gmail.com>
2015-08-18 09:43:17 +0200

    favicon

--- view/Head.php
+++ view/Head.php
@@ -39,6 +39,7 @@ class Head
     <!-- Metadata -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon.ico" /> 
     <link rel="stylesheet" type="text/css" href="css/fonts.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
