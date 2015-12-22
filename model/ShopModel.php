34badbe8ec534ad173e73f21c500fe5d079c1c73
Fabio Colella <fcole90@gmail.com>
2015-12-22 17:18:48 +0100

    Added generic view and shop model

---
+++ model/ShopModel.php
@@ -0,0 +1,117 @@
+<?php
+relRequire("model/DBModel.php");
+/*
+ * Copyright (C) 2015 fabio
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+/**
+ * Description of shopModel
+ *
+ * @author fabio
+ */
+class ShopModel extends DBModel
+{
+    
+    public function __construct() 
+    {
+        parent::__construct();
+    }    
+    
+    public function addShopToDatabase($data)
+    {
+        
+        /** Remove unsetted values from submission **/
+        foreach ($data as $field => $value)
+        {
+            if (!isset($value) || $value == "")
+            {
+                unset($data[$field]);
+            }
+        }
+        try
+        {
+            $mysqli = $this->connect();
+        } 
+        catch (Exception $e) 
+        {
+            $this->error[] = $e->getMessage();
+            $this->error[] = "Could not connect to database.";
+            return false;
+        }
+        
+        $sFields = ""; // used to bind param in ssss like string
+        // dynamically create the statement
+        $text = "INSERT INTO shop (";
+        foreach ($data as $field => $value)
+        {
+            $text .= "$field, ";
+            $sFields .= "s";
+        }
+        $text = rtrim($text, ", "); //remove last comma
+        $text .= ") ";
+        $text .= "values (";
+        foreach ($data as $field)
+        {
+            $text .= "?, ";
+        }
+        $text = rtrim($text, ", "); //remove last comma
+        $text .= ") ";
+        
+        // Create the list of the params for bind_param
+        $paramList[] = $sFields;
+        foreach ($data as $key => $value)
+        {
+            $paramList[] = $value;
+        }
+        
+        if (!$stmt = $mysqli->prepare($text))
+        {
+            $this->error[] = "Error: could not prepare statement: $text";
+            return false;
+        }
+        
+        if (!is_callable(array($stmt, 'bind_param')))
+        {
+             $this->error[] = "Error: stmt is not callable";
+             return false;
+        }
+        
+        //the params need to be passed by reference
+        foreach ($paramList as &$param)
+        {
+            $refParamList[] = &$param;
+        }
+        //call bind param with a dynamic number of params
+        if (!call_user_func_array(array($stmt, 'bind_param'), $refParamList))
+        {
+            $this->error[] = "DB Error: could not bind parameters.";
+            return false;
+        }
+        
+        if (!$stmt->execute())
+        {
+            $this->error[] = "DB Error: could not execute the statement.";
+            return false;
+        }
+        $stmt->close();
+        $mysqli->close();
+        
+        return true;
+        
+        
+    }
+}
---
+++ view/GenericViews.php
@@ -0,0 +1,47 @@
+<?php
+
+/* 
+ * Copyright (C) 2015 fabio
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+class GenericView
+{
+    
+    /**
+     * Get the HTML to render the profile.
+     * 
+     * @param profile string[]
+     * @return string HTML view
+     */
+    public function getProfileView($user)
+    {
+        $text = "<table>\n\t";
+        
+        foreach ($user->fieldlist() as $item)
+        {
+            $value = $user->get($item);
+            if (isset($value) && $item != "password")
+            {
+                $text += "<tr><td>$item</td><td>$value</td></tr>";
+            }
+        }        
+        $text += "\n<table>";
+          
+        return $text;
+    }
+}
+
