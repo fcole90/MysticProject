8a498f1cd2a2cf1abc2c6584a7bca4dd7b82769c
Fabio Colella <fcole90@gmail.com>
2021-03-24 12:49:01 +0200

    Fix mySQLi

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b066ab0fa1b35e52d559e59ba2fbb8ef7d13b743',
+    'name' => 'fcole90/fisherman-locator',
+  ),
+  'versions' => 
+  array (
+    'fcole90/fisherman-locator' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b066ab0fa1b35e52d559e59ba2fbb8ef7d13b743',
+    ),
+  ),
+);
